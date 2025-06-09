fa4a82985e3c4c08eebaa5d63c6b4c2999bc5701
JemmyKibandi <jemimakibandi378@gmail.com>
2025-06-09 14:29:43 +0400

    chore: Editing out a piece of comments

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,11 +5,9 @@ const router = express.Router();
 const User = require("../models/User");
 const auth = require("../middleware/auth");
 
-
 // Get contacts
 router.get("/users", async (req, res) => {
   try {
-    // You can exclude the current user by filtering out req.user.userId if you want
     const users = await User.find({}, "username");
     res.json(users);
   } catch (error) {
@@ -23,7 +21,8 @@ router.post("/register", async (req, res) => {
 
   try {
     const existingUser = await User.findOne({ username });
-    if (existingUser) return res.status(400).json({ message: "Username already exists" });
+    if (existingUser)
+      return res.status(400).json({ message: "Username already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, password: hashedPassword });
@@ -44,21 +43,23 @@ router.post("/login", async (req, res) => {
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
     res.json({
       token,
       user: {
         id: user._id,
-        username: user.username
-      }
+        username: user.username,
+      },
     });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
 });
-
 
 module.exports = router;
